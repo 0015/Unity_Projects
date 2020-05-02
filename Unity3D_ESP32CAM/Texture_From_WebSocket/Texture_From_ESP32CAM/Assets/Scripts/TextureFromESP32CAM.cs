a737be3ec5a8733f69b4b13c2004689780fb9506
Eric <luv.ilho@gmail.com>
2020-05-01 19:51:10 -0700

    Unity3D | ESP32CAM - FROM REALITY TO VIRTUAL (Ft. WebSocket)

---
+++ Unity3D_ESP32CAM/Texture_From_WebSocket/Texture_From_ESP32CAM/Assets/Scripts/TextureFromESP32CAM.cs
@@ -0,0 +1,48 @@
+﻿using NativeWebSocket;
+using UnityEngine;
+
+public class TextureFromESP32CAM : MonoBehaviour
+{
+    private WebSocket _webSocket;
+
+    // Start is called before the first frame update
+    async void Start()
+    {
+        _webSocket = new WebSocket("ws://192.168.1.124:8888");
+        _webSocket.OnOpen += () => { print("Connection Open!"); };
+        _webSocket.OnError += (e) => { print("Error :" + e); };
+        _webSocket.OnClose += (e) => { print("Connection Close!"); };
+        _webSocket.OnMessage += (bytes) =>
+        {
+            print("onMessage length :" + bytes.Length);
+            if (bytes.Length > 0)
+            {
+                Texture2D tex = new Texture2D(2, 2);
+                tex.LoadImage(bytes);
+
+                // var pix = tex.GetPixels32();
+                // Array.Reverse(pix);
+                //
+                // var destTex = new Texture2D(tex.width, tex.height);
+                // destTex.SetPixels32(pix);
+                // destTex.Apply();
+
+                if (GetComponent<Renderer>() != null)
+                {
+                    GetComponent<Renderer>().material.mainTexture = tex;
+                }
+            }
+        };
+        await _webSocket.Connect();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private async void OnApplicationQuit()
+    {
+        await _webSocket.Close();
+    }
+}
