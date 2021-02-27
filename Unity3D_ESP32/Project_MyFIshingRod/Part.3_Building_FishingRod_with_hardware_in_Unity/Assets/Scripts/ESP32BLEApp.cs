f48762e0d31aa1ffd12ce48a98482abf1b925a06
Eric <luv.ilho@gmail.com>
2021-02-27 13:38:20 -0800

    Part.3_Building_FishingRod_with_hardware_in_Unity

---
+++ Unity3D_ESP32/Project_MyFIshingRod/Part.3_Building_FishingRod_with_hardware_in_Unity/Assets/Scripts/ESP32BLEApp.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class ESP32BLEApp : MonoBehaviour
+{
+    private string DeviceName = "ESP32 THAT PROJECT";
+    private string ServiceUUID = "FFE0";
+    private string Characteristic = "FFE1";
+
+
+    enum States
+    {
+        None,
+        Scan,
+        Connect,
+        Subscribe,
+        Unsubscribe,
+        Disconnect,
+        Communication,
+    }
+
+    private bool _connected = false;
+    private float _timeout = 0f;
+    private States _state = States.None;
+    private bool _foundID = false;
+    private string _deviceAddress;
+
+    [SerializeField] private TMP_Text stateText;
+    private Quaternion rawQt;
+    private float _waitingStableTime = 0f;
+
+    void Reset()
+    {
+        _connected = false;
+        _timeout = 0f;
+        _state = States.None;
+        _foundID = false;
+        _deviceAddress = null;
+    }
+
+    void SetState(States newState, float timeout)
+    {
+        _state = newState;
+        _timeout = timeout;
+    }
+
+    void setStateText(string text)
+    {
+        if (stateText == null) return;
+        stateText.text = text;
+    }
+
+    void StartProcess()
+    {
+        setStateText("Initializing...");
+
+        Reset();
+        BluetoothLEHardwareInterface.Initialize(true, false, () =>
+        {
+            SetState(States.Scan, 0.1f);
+            setStateText("Initialized");
+        }, (error) => { BluetoothLEHardwareInterface.Log("Error: " + error); });
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        StartProcess();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_timeout > 0f)
+        {
+            _timeout -= Time.deltaTime;
+            if (_timeout <= 0f)
+            {
+                _timeout = 0f;
+
+                switch (_state)
+                {
+                    case States.None:
+                        break;
+
+                    case States.Scan:
+                        setStateText("Scanning for ESP32 devices...");
+
+                        BluetoothLEHardwareInterface.ScanForPeripheralsWithServices(null, (address, name) =>
+                        {
+                            // we only want to look at devices that have the name we are looking for
+                            // this is the best way to filter out devices
+                            if (name.Contains(DeviceName))
+                            {
+                                // it is always a good idea to stop scanning while you connect to a device
+                                // and get things set up
+                                BluetoothLEHardwareInterface.StopScan();
+
+                                // add it to the list and set to connect to it
+                                _deviceAddress = address;
+                                SetState(States.Connect, 0.5f);
+
+                                _waitingStableTime = 3;
+                            }
+                        }, null, false, false);
+                        break;
+
+                    case States.Connect:
+                        // set these flags
+                        _foundID = false;
+
+                        setStateText("Connecting to ESP32");
+
+                        // note that the first parameter is the address, not the name. I have not fixed this because
+                        // of backwards compatiblity.
+                        // also note that I am note using the first 2 callbacks. If you are not looking for specific characteristics you can use one of
+                        // the first 2, but keep in mind that the device will enumerate everything and so you will want to have a timeout
+                        // large enough that it will be finished enumerating before you try to subscribe or do any other operations.
+                        BluetoothLEHardwareInterface.ConnectToPeripheral(_deviceAddress, null, null,
+                            (address, serviceUUID, characteristicUUID) =>
+                            {
+                                if (IsEqual(serviceUUID, ServiceUUID))
+                                {
+                                    // if we have found the characteristic that we are waiting for
+                                    // set the state. make sure there is enough timeout that if the
+                                    // device is still enumerating other characteristics it finishes
+                                    // before we try to subscribe
+                                    if (IsEqual(characteristicUUID, Characteristic))
+                                    {
+                                        _connected = true;
+                                        SetState(States.Subscribe, 2f);
+                                        setStateText("Connected to ESP32");
+                                    }
+                                }
+                            }, (disconnectedAddress) =>
+                            {
+                                BluetoothLEHardwareInterface.Log("Device disconnected: " + disconnectedAddress);
+                                setStateText("Disconnected");
+                                StartProcess();
+                            });
+                        break;
+
+                    case States.Subscribe:
+                        setStateText("Subscribing to ESP32");
+
+                        BluetoothLEHardwareInterface.SubscribeCharacteristicWithDeviceAddress(_deviceAddress,
+                            ServiceUUID,
+                            Characteristic, null,
+                            (address, characteristicUUID, bytes) =>
+                            {
+                                float qx = BitConverter.ToSingle(bytes, 0);
+                                float qy = BitConverter.ToSingle(bytes, 4);
+                                float qz = BitConverter.ToSingle(bytes, 8);
+                                float qw = BitConverter.ToSingle(bytes, 12) * -1;
+
+                                if (_waitingStableTime <= 0)
+                                {
+                                    FindObjectOfType<ReelController>()
+                                        .updateQuaternion(new Quaternion(qz, -qx, qy, qw));
+                                }
+
+                                _waitingStableTime -= Time.deltaTime;
+                            });
+
+                        // set to the none state and the user can start sending and receiving data
+                        _state = States.None;
+                        break;
+
+                    case States.Unsubscribe:
+                        BluetoothLEHardwareInterface.UnSubscribeCharacteristic(_deviceAddress, ServiceUUID,
+                            Characteristic,
+                            null);
+                        SetState(States.Disconnect, 4f);
+                        break;
+
+                    case States.Disconnect:
+                        if (_connected)
+                        {
+                            BluetoothLEHardwareInterface.DisconnectPeripheral(_deviceAddress, (address) =>
+                            {
+                                BluetoothLEHardwareInterface.DeInitialize(() =>
+                                {
+                                    _connected = false;
+                                    _state = States.None;
+                                });
+                            });
+                        }
+                        else
+                        {
+                            BluetoothLEHardwareInterface.DeInitialize(() => { _state = States.None; });
+                        }
+
+
+                        break;
+                }
+            }
+        }
+    }
+
+    string FullUUID(string uuid)
+    {
+        return "0000" + uuid + "-0000-1000-8000-00805F9B34FB";
+    }
+
+    bool IsEqual(string uuid1, string uuid2)
+    {
+        if (uuid1.Length == 4)
+            uuid1 = FullUUID(uuid1);
+        if (uuid2.Length == 4)
+            uuid2 = FullUUID(uuid2);
+
+        return (uuid1.ToUpper().Equals(uuid2.ToUpper()));
+    }
+}
---
+++ Unity3D_ESP32/Project_MyFIshingRod/Part.3_Building_FishingRod_with_hardware_in_Unity/Assets/Scripts/ReelController.cs
@@ -0,0 +1,91 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ReelController : MonoBehaviour
+{
+    [SerializeField] private TMP_Text stateText;
+    [SerializeField] private Transform reel;
+    [SerializeField] private Transform body;
+    [SerializeField] private GameObject endCube;
+    private Quaternion rawQuat;
+    private Quaternion quatLookAtCam;
+    private float rotatedAroundX;
+    private Vector3 lastUp;
+    private Vector3 lastForward;
+    private float previousBodyY;
+    private float totalAmountAngeY;
+    private Rigidbody endCubeRB;
+    private float gravityScale = 10.0f; // 10 times gravity
+    private static float globalGravity = -9.81f;
+
+
+    private void Start()
+    {
+        endCubeRB = endCube.GetComponent<Rigidbody>();
+        quatLookAtCam = Quaternion.identity;
+        previousBodyY = 0;
+        totalAmountAngeY = 0;
+    }
+
+    public void updateQuaternion(Quaternion _rawQuat)
+    {
+        rawQuat = _rawQuat;
+        var calcRotation = quatLookAtCam * rawQuat;
+
+        transform.rotation = calcRotation;
+        var rotationDifferenceX = Vector3.SignedAngle(transform.up, lastUp, transform.right);
+        rotatedAroundX += rotationDifferenceX;
+
+        var rotationDifferenceY = Vector3.SignedAngle(transform.forward, lastForward, transform.up);
+
+        if (Mathf.Abs(rotationDifferenceX) < 0.1 && Mathf.Abs(rotationDifferenceY) < 0.1)
+        {
+            setStateText("Idle");
+        }
+        else if (Mathf.Abs(rotationDifferenceX) * 2 > Mathf.Abs(rotationDifferenceY))
+        {
+            setStateText("Rotating Reel");
+            reel.transform.localEulerAngles = new Vector3(rotatedAroundX, 0, 0);
+        }
+        else
+        {
+            setStateText("Rotating Body");
+            float amountMove = previousBodyY - transform.eulerAngles.y;
+            float amountMoveWithSign = Mathf.Abs(amountMove) * rotationDifferenceY > 0 ? -1 : 1;
+
+            if (Mathf.Abs(amountMove) >= 0.1f)
+            {
+                if (endCubeRB != null)
+                {
+                    endCubeRB.velocity = endCubeRB.velocity * 0.9f;
+                    endCubeRB.angularVelocity = endCubeRB.angularVelocity * 0.9f;
+                    Vector3 gravity = globalGravity * gravityScale * Vector3.up;
+                    endCubeRB.AddForce(gravity, ForceMode.Acceleration);
+                }
+
+                var bodyY = body.eulerAngles.y + amountMoveWithSign;
+                body.localEulerAngles = new Vector3(-90, bodyY, 0);
+            }
+
+            previousBodyY = transform.eulerAngles.y;
+        }
+
+
+        lastUp = transform.up;
+        lastForward = transform.forward;
+    }
+
+    public void ResetRotation()
+    {
+        quatLookAtCam = Quaternion.Inverse(rawQuat);
+        totalAmountAngeY = 0;
+        previousBodyY = 0;
+        body.localEulerAngles = new Vector3(-90, 0, 0);
+    }
+
+    void setStateText(string text)
+    {
+        if (stateText == null) return;
+        stateText.text = "Status: " + text;
+    }
+}
